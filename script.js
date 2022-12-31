a845694f16005282cd9251551fe4c61b7c38ffc6
Austin-Kindig <kindig.austin.ak@gmail.com>
2022-12-30 21:25:43 -0500

    rough out HTML and CSS

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+const myLibrary = [];
+
+// object constructor function
+function Book(title, author, pages, isRead) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+}
+
+function addBookToLibrary() {
+
+}

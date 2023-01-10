ddf252f9cb07d0dbb3a5a1224e98400ab36e2afa
Austin Kindig <kindig.austin.ak@gmail.com>
2023-01-10 10:46:45 -0500

    clean up code

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// clean up some fucntions and separate jobs/refactor
 // add a book delete button
 // add a book is read button
+const bookShelf = document.querySelector('.books-container')
+const books = document.querySelectorAll('.book-card')
 
 document.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -8,14 +11,15 @@ document.addEventListener('submit', (e) => {
     input.isRead === 'on' ? input.isRead = 'yes' : input.isRead = 'no'
     document.querySelector('form').reset()
     myLibrary.push(input)
-    for(let i = myLibrary.length; i > 0; i--) {
-
-        document.querySelector('.books-container').appendChild(extractBooks(i - 1, myLibrary[i - 1]))
-    
-    }
+    refreshDisplay()
 
 })
 
+books.forEach((book) =>
+  book.addEventListener("click", () => {
+    bookShelf.removeChild(bookShelf.querySelector())
+  })
+);
 // initialize book object storage
 const myLibrary = []
 
@@ -34,13 +38,13 @@ function addBookToLibrary() {
 }
 // converts book objects into DOM lists
 function extractBooks (index, book) {
-    const cardDiv = createDiv('book-card', index + 1, '')
+    const cardDiv = createDiv('book-card', `card-${index + 1}`, '')
     cardDiv.appendChild(createDiv('title', null, `Title: ${book.title}`))
     cardDiv.appendChild(createDiv('author', null, `Author: ${book.author}`))
     cardDiv.appendChild(createDiv('pages', null, `Number of pages: ${book.pages}`))
     cardDiv.appendChild(createDiv('isRead', null, `Have I read it yet? ${book.isRead}`))
     cardDiv.appendChild(createButton('change-isRead-button', null, 'Finished Book'))
-    cardDiv.appendChild(createButton('remove-book-button', `remove-button-${index + 1}`, 'Remove Book'))
+    cardDiv.appendChild(createButton('remove-book-button', index + 1, 'Remove Book'))
     return cardDiv
 }
 
@@ -53,10 +57,20 @@ function createDiv (className, id, text) {
     return newElement
 }
 
+// creates 'button' DOM element
 function createButton (className, id, text) {
     const newElement = document.createElement('button')
     newElement.setAttribute('id', id)
     newElement.setAttribute('class', className)
     newElement.innerText = text
     return newElement
+}
+
+// reads the library array and updates the book shelf to match its contents
+function refreshDisplay () {
+    for(let i = myLibrary.length; i > 0; i--) {
+
+        bookShelf.appendChild(extractBooks(i - 1, myLibrary[i - 1]))
+    
+    }
 }
